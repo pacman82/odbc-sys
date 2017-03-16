aa856d9a09b775aa53a2d6842b5ccbbba5c93a32
Markus Klein <markus-klein@live.de>
2017-03-16 21:15:39 +0100

    ignore empty DYLD_LIBRARY_PATH s

--- build.rs
+++ build.rs
@@ -3,17 +3,19 @@ fn main() {
         // if we're on Mac OS X we'll kindly add DYLD_LIBRARY_PATH to rustc's
         // linker search path
         if let Some(dyld_paths) = option_env!("DYLD_LIBRARY_PATH") {
-            for path in dyld_paths.split(':') {
-                println!("cargo:rustc-link-search=native={}", path)
-            }
+            print_paths(dyld_paths);
         }
         // if we're on Mac OS X we'll kindly add DYLD_FALLBACK_LIBRARY_PATH to rustc's
         // linker search path
         if let Some(dyld_fallback_paths) = option_env!("DYLD_FALLBACK_LIBRARY_PATH") {
-            for path in dyld_fallback_paths.split(':') {
-                println!("cargo:rustc-link-search=native={}", path)
-            }
+            print_paths(dyld_fallback_paths);
         }
+    }
+}
+
+fn print_paths(paths: &str) {
+    for path in paths.split(':').filter(|x| x.is_empty()) {
+        println!("cargo:rustc-link-search=native={}", path)
     }
 }
 
