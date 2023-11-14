bd6a0351c265c77f048017f68fe29279a9750908
Lars Francke <lars.francke@gmail.com>
2023-11-14 23:06:06 +0100

    Maybe fix conditional link attributes

--- src/functions.rs
+++ src/functions.rs
@@ -988,7 +988,23 @@ extern "system" {
     pub fn SQLParamData(hstmt: HStmt, value_out: *mut Pointer) -> SqlReturn;
 }
 
-#[link(name = "odbcinst")]
+#[cfg_attr(windows, link(name = "odbc32"))]
+#[cfg_attr(
+    all(not(windows), not(feature = "static"), not(feature = "iodbc")),
+    link(name = "odbcinst")
+)]
+#[cfg_attr(
+    all(not(windows), feature = "static", not(feature = "iodbc")),
+    link(name = "odbcinst", kind = "static")
+)]
+#[cfg_attr(
+    all(not(windows), not(feature = "static"), feature = "iodbc"),
+    link(name = "iodbcinst")
+)]
+#[cfg_attr(
+    all(not(windows), feature = "static", feature = "iodbc"),
+    link(name = "iodbcinst", kind = "static")
+)]
 extern "C" {
     ///  Gets a list of names of values or data corresponding to a value of the system information.
     ///
