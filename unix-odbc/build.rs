483a41a3c2daec412558f616d33fbf087a6eb702
Markus Klein <markus-klein@live.de>
2025-10-12 15:24:01 +0200

    revert: Revert "fix: unixODBC builds completly out of source"
    This reverts commit d94e62fcceaac99b60acc70827d2ee1d54a30b1c.
    
    Accidential commit, breaks build

--- unix-odbc/build.rs
+++ unix-odbc/build.rs
@@ -1,22 +1,16 @@
-use std::{env, path::PathBuf};
-
 use autotools::Config;
-use fs_extra::dir::{copy, CopyOptions};
 
 fn main() {
     if cfg!(target_os = "windows") {
         return;
     }
 
-    // OUT_DIR is an environment provided by cargo. It points to a temporary build directory. This
-    // allows us to keep our source tree clean.
-    let out_dir: PathBuf = env::var("OUT_DIR").unwrap().parse().unwrap();
-    let unix_odbc_src = out_dir.join("unixODBC");
-
-    copy("unixODBC", &unix_odbc_src, &CopyOptions::new()).unwrap();
-
-    let path = Config::new(&unix_odbc_src)
+    let path = Config::new("unixODBC")
         .cflag("-std=gnu99")
+        // Prevent recreating the configure script
+        .env("ACLOCAL", "true")
+        .env("AUTOMAKE", "true")
+        .env("AUTOCONF", "true")
         .build();
     let path = path.join("lib");
     println!("cargo:rustc-link-search=native={}", path.display());
