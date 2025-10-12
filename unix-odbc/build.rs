9f69ab8960a776ac9b1a2d536f6bb05f1964b024
lovasoa <contact@ophir.dev>
2025-10-13 00:29:04 +0200

    remove dependency to autotools
    this makes the build faster, removes exotic build time dependencies on some platforms, works around bugs in autotools, makes the build process easier to troubleshoot, and avoids having to copy the entire unixodbc source tree to OUT_DIR at build time.

--- unix-odbc/build.rs
+++ unix-odbc/build.rs
@@ -1,30 +1,55 @@
-use std::{env, path::PathBuf};
-
-use autotools::Config;
-use fs_extra::dir::{CopyOptions, copy};
+use std::fs;
+use std::path::{Path, PathBuf};
 
 fn main() {
-    if cfg!(target_os = "windows") {
-        return;
+    let vendor_dir = Path::new("./unixODBC");
+
+    let mut build = cc::Build::new();
+
+    build.includes([vendor_dir, vendor_dir.parent().unwrap()]);
+    let header_dirs = ["include", "DriverManager", "odbcinst", "ini", "log", "lst"];
+    build.includes(header_dirs.iter().map(|s| vendor_dir.join(s)));
+
+    setup_compiler_flags(&mut build);
+
+    let configure_ac = vendor_dir.join("configure.ac");
+    build.define("VERSION", Some(unixodbc_version(&configure_ac).as_str()));
+
+    let source_dirs = ["DriverManager", "odbcinst", "ini", "log", "lst"];
+    build.files(c_files_in_dirs(vendor_dir, &source_dirs));
+
+    build.compile("odbc");
+    println!("cargo:rerun-if-changed={}", vendor_dir.display());
+}
+
+fn unixodbc_version(configure_ac_path: &Path) -> String {
+    use std::{fs::File, io::BufRead, io::BufReader};
+    let content = File::open(configure_ac_path).expect("Failed to read configure.ac");
+
+    for line in BufReader::new(content).lines() {
+        let line = line.expect("Failed to read line from configure.ac");
+        if line.trim_start().starts_with("AC_INIT") {
+            let version = line.split(['[', ']']).nth(2);
+            if let Some(version) = version {
+                return format!("\"{}\"", version);
+            }
+        }
     }
+    panic!("Failed to find version in configure.ac");
+}
 
-    // OUT_DIR is an environment provided by cargo. It points to a temporary build directory. This
-    // allows us to keep our source tree clean.
-    let out_dir: PathBuf = env::var("OUT_DIR").unwrap().parse().unwrap();
-    let options = CopyOptions {
-        overwrite: true,
-        ..Default::default()
-    };
-    copy("unixODBC", &out_dir, &options).unwrap();
-    let unix_odbc_src = out_dir.join("unixODBC");
+fn c_files_in_dirs(base_dir: &Path, paths: &[&str]) -> Vec<PathBuf> {
+    paths
+        .iter()
+        .map(|p| base_dir.join(p))
+        .flat_map(|dir| fs::read_dir(dir).expect("read source directory"))
+        .map(|p| p.expect("read source file").path())
+        .filter(|p| p.extension().unwrap_or_default() == "c")
+        .collect()
+}
 
-    let path = Config::new(&unix_odbc_src)
-        .cflag("-std=gnu99")
-        // Prevent recreating the configure script
-        .env("ACLOCAL", "true")
-        .env("AUTOMAKE", "true")
-        .env("AUTOCONF", "true")
-        .build();
-    let path = path.join("lib");
-    println!("cargo:rustc-link-search=native={}", path.display());
+fn setup_compiler_flags(build: &mut cc::Build) {
+    build.flag_if_supported("-std=gnu99");
+    build.flag_if_supported("-Wno-pointer-to-int-cast");
+    build.warnings(false);
 }
