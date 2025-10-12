3152d2e0a53e855f197d045fb0ef9ae1014b4465
lovasoa <contact@ophir.dev>
2025-10-13 00:59:53 +0200

    simplify build script
    removing unnecessary functions
    
    consolidating the logic for including directories and defining the version.
    
    The build now directly reads the version from configure.ac and compiles the source files more directly

--- unix-odbc/build.rs
+++ unix-odbc/build.rs
@@ -1,55 +1,38 @@
-use std::fs;
+use std::fs::{self, read_to_string};
 use std::path::{Path, PathBuf};
 
 fn main() {
-    let vendor_dir = Path::new("./unixODBC");
+    let vendor_dir = Path::new("unixODBC");
+    let src_dirs: Vec<PathBuf> = ["DriverManager", "odbcinst", "ini", "log", "lst"]
+        .into_iter()
+        .map(|s| vendor_dir.join(s))
+        .collect();
+
+    let configure_ac_path = vendor_dir.join("configure.ac");
+    let configure_ac = read_to_string(configure_ac_path).expect("Failed to read configure.ac");
+    let version = configure_ac
+        .split("AC_INIT([unixODBC], [")
+        .nth(1)
+        .expect("Failed to find version in configure.ac")
+        .split(']')
+        .next()
+        .expect("Unparsable version in configure.ac");
+    let version_str = format!("\"{}\"", version);
 
     let mut build = cc::Build::new();
-
-    build.includes([vendor_dir, vendor_dir.parent().unwrap()]);
-    let header_dirs = ["include", "DriverManager", "odbcinst", "ini", "log", "lst"];
-    build.includes(header_dirs.iter().map(|s| vendor_dir.join(s)));
-
-    setup_compiler_flags(&mut build);
-
-    let configure_ac = vendor_dir.join("configure.ac");
-    build.define("VERSION", Some(unixodbc_version(&configure_ac).as_str()));
-
-    let source_dirs = ["DriverManager", "odbcinst", "ini", "log", "lst"];
-    build.files(c_files_in_dirs(vendor_dir, &source_dirs));
-
-    build.compile("odbc");
-    println!("cargo:rerun-if-changed={}", vendor_dir.display());
-}
-
-fn unixodbc_version(configure_ac_path: &Path) -> String {
-    use std::{fs::File, io::BufRead, io::BufReader};
-    let content = File::open(configure_ac_path).expect("Failed to read configure.ac");
-
-    for line in BufReader::new(content).lines() {
-        let line = line.expect("Failed to read line from configure.ac");
-        if line.trim_start().starts_with("AC_INIT") {
-            let version = line.split(['[', ']']).nth(2);
-            if let Some(version) = version {
-                return format!("\"{}\"", version);
-            }
-        }
-    }
-    panic!("Failed to find version in configure.ac");
-}
-
-fn c_files_in_dirs(base_dir: &Path, paths: &[&str]) -> Vec<PathBuf> {
-    paths
-        .iter()
-        .map(|p| base_dir.join(p))
-        .flat_map(|dir| fs::read_dir(dir).expect("read source directory"))
-        .map(|p| p.expect("read source file").path())
-        .filter(|p| p.extension().unwrap_or_default() == "c")
-        .collect()
-}
-
-fn setup_compiler_flags(build: &mut cc::Build) {
-    build.flag_if_supported("-std=gnu99");
-    build.flag_if_supported("-Wno-pointer-to-int-cast");
-    build.warnings(false);
+    build
+        .includes([vendor_dir, Path::new("."), &vendor_dir.join("include")])
+        .includes(&src_dirs)
+        .files(
+            src_dirs
+                .iter()
+                .flat_map(|dir| fs::read_dir(dir).expect("read source directory"))
+                .map(|p| p.expect("read source file").path())
+                .filter(|p| p.extension().unwrap_or_default() == "c"),
+        )
+        .define("VERSION", version_str.as_str())
+        .flag_if_supported("-std=gnu99")
+        .flag_if_supported("-Wno-pointer-to-int-cast")
+        .warnings(false)
+        .compile("odbc");
 }
